4de8ed5bcfe638e077112e09ce3df6ba3f47ddab
Justin Lazarski <blackhawks189@gmail.com>
2019-04-05 16:31:13 -0500

    First commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+/*
+Justin Lazarski
+Jonathan La Rosa
+Team Project 1: Question 2 Palidrome Tester
+CIS 3090.04
+4/5/19
+ */
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        
+    }
+}
