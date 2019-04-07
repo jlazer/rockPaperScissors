a230080ce36e8c299a1e0e04c5a39ede8b77e006
Justin Lazarski <blackhawks189@gmail.com>
2019-04-06 19:32:26 -0500

    Finished. The rock paper scissors game works as it should.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,26 +14,34 @@ public class Main {
     public static String computerChoice() {
 
         String computerChoice = "";
+
         Random randomNum = new Random();
         int num = randomNum.nextInt(3);
         num += 1;
-        System.out.println("Random number: " + num);
+
+        //System.out.println("Random number: " + num);
 
         if (num == 1) {
+
             computerChoice = "rock";
         } else if (num == 2) {
+
             computerChoice = "paper";
         } else if (num == 3) {
+
             computerChoice = "scissors";
         }
-        System.out.println("Computer Choice: " + computerChoice);
+
+
         return computerChoice;
     }
 
 
     public static String userChoice(String choice) {
-        String userChoice = choice;
+
         Scanner input = new Scanner(System.in);
+
+        String userChoice = choice;
         userChoice = userChoice.toLowerCase();
         System.out.println("User Choice: " + userChoice);
 
@@ -45,66 +53,74 @@ public class Main {
         }
         return userChoice;
     }
+
     public static boolean isValidChoice(String choice) {
 
         return (choice.equalsIgnoreCase("rock")) || (choice.equalsIgnoreCase("paper")) || (choice.equalsIgnoreCase("scissors"));
 
     }
-    public static void determineWinner(String computer, String user) {
+
+    public static String determineWinner(String computer, String user) {
+
         String winner;
-        System.out.println("hello from determinewinner method");
+        winner = "";
+
+        //System.out.println("hello from determineWinner method");
+
         String userChoice = user;
         String computerChoice = computer;
 
-        if (userChoice == "rock" && computerChoice == "scissors") {
+        if (userChoice.equals("rock") && computerChoice.equals("scissors")) {
+
             winner = "Rock versus Scissors..You win!";
-        } else if (userChoice == "scissors" && computerChoice == "rock") {
+
+        } else if (userChoice.equals("scissors") && computerChoice.equals("rock")) {
+
             winner = "Scissors versus Rock..You Lose!";
-        } else if (userChoice == "rock" && computerChoice == "paper") {
+
+        } else if (userChoice.equals("rock") && computerChoice.equals("paper")) {
+
             winner = "Rock versus Paper..You Lose!";
-        } else if (userChoice == "paper" && computerChoice == "rock") {
+
+        } else if (userChoice.equals("paper") && computerChoice.equals("rock")) {
+
             winner = "Paper versus Rock..You Win!";
-        } else if (userChoice == "paper" && computerChoice == "scissors") {
+
+        } else if (userChoice.equals("paper") && computerChoice.equals("scissors")) {
+
             winner = "Paper versus Scissors..You lose!";
-        } else if (userChoice == "scissors" && computerChoice == "paper") {
+
+        } else if (userChoice.equals("scissors") && computerChoice.equals("paper")) {
+
             winner = "Scissors versus Paper..You Win!";
-        } else {
-            if (userChoice == "rock" && computerChoice == "rock") {
-                winner = "Rock versus Rock...Tie!";
-            } else if (userChoice == "paper" && computerChoice == "paper") {
-                winner = "Paper versus Paper...Tie!";
-            } else if (userChoice == "scissors" && computerChoice == "scissors") {
-                winner = "Scissors versus Scissors...Tie!";
-            }
+
+        } else if (userChoice.equals(computerChoice)) {
+
+                winner = userChoice + " versus " + computerChoice + "...Tie!";
+
         }
-        
+        return winner;
     }
 
-
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
 
-        String userInput;
+        String userInput = "";
         String computerInput = "";
+
         while(true) {
 
             System.out.println("Please enter 'rock' 'paper' or 'scissors'");
-            //userInput = input.nextLine();
+
             userInput = userChoice(input.nextLine());
+
             computerInput = computerChoice();
 
             System.out.println("Computer Choice: " + computerInput);
 
-            determineWinner(computerInput, userInput);
-        }
-        //computerChoice();
-        //System.out.println("Computer Choice: " + computerChoice);
-
-        //System.out.println("Please enter 'rock' 'paper' or 'scissors'");
-        //userInput = input.nextLine();
-
-        //userChoice(userInput);
-        //System.out.println("userInput: " + userInput);
+            System.out.println(determineWinner(computerInput, userInput));
 
         }
     }
+}
